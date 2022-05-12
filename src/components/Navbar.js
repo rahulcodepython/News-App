4a21e5262d88cf953ed3505179a462a893f9373a
rahulcodepython <rd21102004@gmail.com>
2022-05-12 14:11:05 +0530

    Almost Complete Project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,19 +7,19 @@ export default function Navbar(props) {
         <div style={{ "marginBottom": '5rem' }}>
             <nav className={`navbar fixed-top navbar-expand-lg navbar-${props.theme} bg-${props.theme}`}>
                 <div className="container-fluid">
-                    <a className="navbar-brand" style={{'marginLeft': '20rem'}}>Navbar</a>
+                    <Link to='/action' className="navbar-brand" style={{'marginLeft': '20rem'}}>Navbar</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent" >
                         <ul className="nav navbar-nav navbar-center mx-auto">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page">Home</a>
+                                <Link className="nav-link active" to='/action' aria-current="page">Home</Link>
                             </li>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <Link to='/action' className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Dropdown
-                                </a>
+                                </Link>
                                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                                     <li><Link className="dropdown-item" to="/">General</Link></li>
                                     <li><Link className="dropdown-item" to="/technology">Technology</Link></li>
@@ -37,11 +37,11 @@ export default function Navbar(props) {
                                 <Link className="nav-link" to="/Blog">Blog</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" style={{"cursor": 'pointer'}}>
+                                <Link className="nav-link" to='/action' style={{"cursor": 'pointer'}}>
                                     <span className="material-icons" onClick={props.mode}>
                                         {props.theme === 'light' ? 'dark_mode' : 'light_mode'}
                                     </span>
-                                </a>
+                                </Link>
                             </li>
                         </ul>
                     </div>
